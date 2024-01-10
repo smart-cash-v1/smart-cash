183f04b88706834220979936c03e652221436605
Rodrigodev1998 <rodrigofersouza5@gmail.com>
2024-01-10 13:45:32 -0300

    implementado descricao no controller

--- src/main/java/com/dev/smartcash/Companie/application/api/CompanieApi.java
+++ src/main/java/com/dev/smartcash/Companie/application/api/CompanieApi.java
@@ -1,5 +1,7 @@
 package com.dev.smartcash.Companie.application.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,13 +9,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/companie")
+@Tag(name = "Companie", description = "Endpoints relacionados às informações das empresas.")
 public interface CompanieApi {
 
     @PostMapping
+    @Operation(summary = "Cadastra empresa",
+            description = "Este endpoint cadastra uma nova empresa.")
     @ResponseStatus(HttpStatus.CREATED)
     CompanieResponse cadastraCompanie(@RequestBody CompanieRequest companieRequeste);
 
-    @GetMapping
+    @GetMapping("/all-companies")
+    @Operation(summary = "Retorna todas as empresas cadastradas",
+            description = "Este endpoint recupera uma lista de todas as empresas cadastradas no sistema.")
     @ResponseStatus(code = HttpStatus.OK)
     List<CompanieListResponse> getTodasCompanie();
 
